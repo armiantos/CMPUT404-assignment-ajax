dc74b139afa206b591539feaa79940bf0da29980
Armianto Sumitro <armiantos@gmail.com>
2022-03-06 13:30:41 -0700

    Return new world on clear

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ def get_entity(entity):
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
-    return ''
+    return json.dumps(myWorld.world())
 
 if __name__ == "__main__":
     # TODO: Revert this back to defaults

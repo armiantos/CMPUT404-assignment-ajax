cf7817af2635f67f4ea978d757cd56c769c2ca23
Armianto Sumitro <armiantos@gmail.com>
2022-03-06 15:21:55 -0700

    Format, add copyright, remove temporary port host

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Copyright 2022 Armianto Sumitro
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +21,10 @@
 # remember to:
 #     pip install flask
 
-
 import flask
 from flask import Flask, request, redirect
 import json
+
 app = Flask(__name__)
 app.debug = True
 
@@ -33,12 +34,14 @@ app.debug = True
 #    'b':{'x':2, 'y':3}
 # }
 
+
 class World:
+
     def __init__(self):
         self.clear()
-        
+
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
 
@@ -49,15 +52,17 @@ class World:
         self.space = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
-    
+        return self.space.get(entity, dict())
+
     def world(self):
         return self.space
 
-# you can test your webservice from the commandline
-# curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+# you can test your webservice from the commandline
+# curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
+
+myWorld = World()
+
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -71,12 +76,14 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect('static/index.html')
 
-@app.route("/entity/<entity>", methods=['POST','PUT'])
+
+@app.route("/entity/<entity>", methods=['POST', 'PUT'])
 def update(entity):
     '''update the entities via this interface'''
     data = flask_post_json()
@@ -84,22 +91,26 @@ def update(entity):
         myWorld.update(entity, key, data[key])
     return data
 
-@app.route("/world", methods=['POST','GET'])    
+
+@app.route("/world", methods=['POST', 'GET'])
 def world():
     '''you should probably return the world here'''
     return json.dumps(myWorld.world())
 
-@app.route("/entity/<entity>")    
+
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
     return json.dumps(myWorld.get(entity))
 
-@app.route("/clear", methods=['POST','GET'])
+
+@app.route("/clear", methods=['POST', 'GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
     return json.dumps(myWorld.world())
 
+
 if __name__ == "__main__":
     # TODO: Revert this back to defaults
-    app.run(host="0.0.0.0", port=3000)
+    app.run()
